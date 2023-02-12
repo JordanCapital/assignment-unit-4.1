622904b6e73defddfe3fe7f132f9df74d51e8b87
Jordan Capital <118566815+JordanCapital@users.noreply.github.com>
2023-02-12 23:32:12 +0300

    Update function-practice.js

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -55,3 +55,62 @@ console.log(positiveResult);
 
 negativeResult = isPositive(-2);
 console.log(negativeResult);
+
+// Call the function to test each outcome (true & false) 
+// Write a separate console.log statement for each outcome
+function isPositive(number){
+  if (number > 0){
+    return true;
+  }
+  else if (number === 0){
+    return false;
+  }
+  else {
+    return false;q2qA2
+  }
+}
+console.log( 'isPositive - should say true', isPositive(3) );
+console.log( 'isPositive - should say false', isPositive(0) );
+console.log( 'isPositive - should say false', isPositive(-3) );
+
+
+// 6. Function to return the _last_ item in an array. If the 
+//    array is empty, return `undefined`.
+function getLast( array ) {
+  if (array.length === 0){
+    return undefined;
+  }
+  return array[array.length - 1];
+}
+let arrayResult =getLast([2, 6, 8, 10, 2, 9]);
+console.log(arrayResult);
+
+arrayResult = getLast([]);
+console.log(arrayResult);
+// 7. Function to find a value in an array. Return true if the 
+//    value is found and false otherwise. Use a loop;
+//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+function find( value, array ){
+  for(let i =0; i < array.length; i++){
+    if(array[i] === value){
+      return true;
+    }
+  }
+  return false;
+}
+let valueResult = find(5,[4, 5, 9, 15, 3, 10]);
+console.log(valueResult);
+
+valueResult = find(1, [4, 5, 9, 15, 3, 10]);
+console.log(valueResult);
+
+// using Array.includes
+
+function find(value, array){
+  return array.includes(value);
+}
+let includeResult = find(2, [1, 2, 3, 4, 5, 6]);
+console.log(includeResult);
+
+includeResult = find(0, [1, 2, 3, 4, 5, 6]);
+console.log(includeResult);
